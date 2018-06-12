135b84f1b9d726f2285b5c165d86b5aacf33129b
pakam2 <pkami.87@gmail.com>
2018-06-12 21:00:31 +0200

    Recipe Model, main view and template

--- cookbook_project/cookbook/models.py
+++ cookbook_project/cookbook/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+
+SEASONS = (  ("SP", "Spring"),
+            ("SU", "Summer"),
+            ("AU", "Autumn"),
+            ("WI","Winter"),
+            )
+class RecipesModel(models.Model):
+
+    recipe_title = models.CharField(max_length=200)
+    recipe_created = models.DateField(auto_now_add = True)
+    recipe_season = models.CharField(max_length=2, choices=SEASONS) 
+
+class IngredientsModel(models.Model):
+    
+    pass
+
+
+
